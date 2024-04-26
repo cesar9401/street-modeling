9baace212501749ffeccd170158d1a0a256216e3
César Tzoc <csartalva01@gmail.com>
2024-04-26 00:46:23 -0600

    suitable function, first version :D

--- geneticalgorithm/suitable_function.py
+++ geneticalgorithm/suitable_function.py
@@ -22,37 +22,32 @@ def calculate_fitness(ind: Individual) -> float:
         in_percentage[in_edge.id] += gen.current_percentage
         out_percentage[out_edge.id] += gen.current_percentage
 
-        if out_edge.id in edge_cars_quantity:
-            cars_in = min(
-                edge_cars_quantity.get(in_edge.id),
-                math.floor(out_edge.capacity * gen.current_percentage / 100)
-            )
-        else:
-            cars_in = math.floor(in_edge.capacity * gen.current_percentage / 100)
+        # cars that could enter
+        cars_in = math.floor(in_edge.capacity * gen.current_percentage / 100)
+        if in_edge.id in edge_cars_quantity:
+            # minimum of cars that could enter and cars in the enter edge
+            cars_in = min(cars_in, edge_cars_quantity.get(in_edge.id))
 
         if out_edge.id not in edge_cars_quantity:
             edge_cars_quantity[out_edge.id] = 0
         edge_cars_quantity[out_edge.id] += cars_in
 
         if not in_edge.from_node:
-            # print(f'in: {in_edge.label} -> {out_edge.label}: {in_edge.capacity} -> {gen.current_percentage}')
             total_in_percentage += gen.current_percentage
             edge_cars_quantity[out_edge.id] = cars_in
             total_cars_in += edge_cars_quantity[out_edge.id]
         if not out_edge.to_node:
-            # print(f'out: {out_edge.label} -> {out_edge.capacity} -> {gen.current_percentage}')
             total_out_percentage += gen.current_percentage
-            total_cars_out += edge_cars_quantity[in_edge.id]
+            # total number of vehicles leaving
+            total_cars_out += cars_in
 
     # more than 100%
     sum_of_in_values = [x for x in list(in_percentage.values()) if x > 100]
     sum_of_out_values = [x for x in list(out_percentage.values()) if x > 100]
 
-    if len(sum_of_in_values) or len(sum_of_out_values):
+    # more than 100% in or out or 0% cars that leave the system
+    if len(sum_of_in_values) or len(sum_of_out_values) or not total_out_percentage:
         return 0
 
-    # no out
-    if not total_out_percentage:
-        return 0
-
+    print(edge_cars_quantity)
     return total_cars_out / total_cars_in
