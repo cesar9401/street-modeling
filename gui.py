7f1436c29c2b90fba8b16e48b109628f4a53ad4e
César Tzoc <csartalva01@gmail.com>
2024-04-29 02:23:53 -0600

    feat: adding nodes and edges.
    - Non functional yet

--- gui.py
+++ gui.py
@@ -12,6 +12,10 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 class GraphWidget(QWidget):
     def __init__(self):
         super(GraphWidget, self).__init__()
+        self.figure = None
+        self.canvas = None
+        self.G = None
+
         font = QFont()
         font.setPointSize(16)
         self.init_ui()
@@ -21,19 +25,19 @@ class GraphWidget(QWidget):
         self.set_center()
         self.setWindowTitle("IA")
 
-        grid = QGridLayout()
-        self.setLayout(grid)
+        self.grid = QGridLayout()
+        self.setLayout(self.grid)
 
         # add buttons here
         button_layout = self.create_buttons()
-        grid.addLayout(button_layout, 0, 0)
+        self.grid.addLayout(button_layout, 0, 0)
 
         # canvas
         self.figure = plt.figure()
         self.canvas = FigureCanvas(self.figure)
-        grid.addWidget(self.canvas, 0, 1, 9, 9)
+        self.grid.addWidget(self.canvas, 0, 1, 9, 9)
 
-        self.make_network()
+        self.G = self.make_network()
 
     def set_center(self):
         qr = self.frameGeometry()
@@ -53,13 +57,13 @@ class GraphWidget(QWidget):
         # add node button
         add_node_btn = QPushButton("Add Node")
         add_node_btn.setObjectName("add_node_btn")
-        add_node_btn.clicked.connect(self.submit_command)
+        add_node_btn.clicked.connect(self.add_node)
         layout.addWidget(add_node_btn)
 
         # add edge button
         add_edge_btn = QPushButton("Add Edge")
         add_edge_btn.setObjectName("add_edge_btn")
-        add_edge_btn.clicked.connect(self.submit_command)
+        add_edge_btn.clicked.connect(self.add_edge)
         layout.addWidget(add_edge_btn)
 
         # remove object button
@@ -73,11 +77,49 @@ class GraphWidget(QWidget):
     def submit_command(self):
         print('Submitted command', self.sender().objectName())
 
+    def add_node(self):
+        print('Submitted command', self.sender().objectName())
+        self.figure = plt.figure()
+        self.canvas = FigureCanvas(self.figure)
+        self.grid.addWidget(self.canvas, 0, 1, 9, 9)
+
+        self.G.add_node('node3', pos=(200, 300))
+
+        pos = nx.get_node_attributes(self.G, 'pos')
+
+        # plt.clf()
+        plt.title('Demo')
+        plt.axis('off')
+
+        nx.draw_networkx(self.G, pos=pos, arrows=True, node_size=2500, alpha=0.85, node_color='c', with_labels=True)
+
+        nx.draw_networkx_edge_labels(
+            self.G, pos=pos, edge_labels={('node1', 'node2'): 'A(100)'}, font_color='black', alpha=0.2
+        )
+
+    def add_edge(self):
+        self.figure = plt.figure()
+        self.canvas = FigureCanvas(self.figure)
+        self.grid.addWidget(self.canvas, 0, 1, 9, 9)
+
+        self.G.add_edge('node1', 'node3', label='B(50)')
+
+        pos = nx.get_node_attributes(self.G, 'pos')
+
+        # plt.clf()
+        plt.title('Demo')
+        plt.axis('off')
+
+        nx.draw_networkx(self.G, pos=pos, arrows=True, node_size=2500, alpha=0.85, node_color='c', with_labels=True)
+
+        nx.draw_networkx_edge_labels(
+            self.G, pos=pos, edge_labels={('node1', 'node2'): 'A(100)'}, font_color='black', alpha=0.2
+        )
+
     def make_network(self):
         g = nx.DiGraph()
         g.add_node('node1', pos=(100, 100))
         g.add_node('node2', pos=(200, 100))
-        g.add_node('node3', pos=(400, 100))
         g.add_edge('node1', 'node2', label='A(100)')
 
         pos = nx.get_node_attributes(g, 'pos')
@@ -88,6 +130,8 @@ class GraphWidget(QWidget):
         )
         plt.title('Demo')
         plt.axis('off')
+
+        return g
 
 
 app = QApplication(sys.argv)
